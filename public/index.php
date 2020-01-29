463b9ca3435744f4fbc8fb590b2bb993b714e87e
ChileNetwork <msotosalgado.77@gmail.com>
2020-01-29 20:06:20 -0300

    Cambio index.php web

--- public/index.php
+++ public/index.php
@@ -1,35 +1,24 @@
-<?php 
-//declare(strict_types=1);
-
+<?php declare(strict_types=1);
 //use App\Factory\RouteFactory;
 //use DI\ContainerBuilder; 
-/*
-* Dependency injection is where components are given their dependencies through their constructor, methods 
+/*Dependency injection is where components are given their dependencies through their constructor, methods 
 * directly into fields;
 */
 //use App\User;
 //use App\SessionStorage;
-
 use App\Container;
 require_once __DIR__ . '/../vendor/autoload.php';
-
-
 //$_SESSION['language'] = 'es';
-
 //$user_language = $_SESSION['language'];
-
 //$user = new User();
 //$user->setLanguage('es');
 //$user_language = $user->getLanguage();
-
 //$storage = new SessionStorage('SESSION_ID_CHILENETWORK');
 //Constructor Injection:
 //$user = new User($storage);//
-
 //Property Injection:
 //$user = new User();//$storage
 //$user->setSessionStorage($storage);
-
 //Using my Container passing parameters
 //$parameters = ['user.session_id'=>'SESSION_ID_HOY'];
 $container = new Container([
@@ -38,6 +27,6 @@ $container = new Container([
 ]);
 $user = $container->getUser();
 
-
 $user->setLanguage('es');
+
 dump($user);
